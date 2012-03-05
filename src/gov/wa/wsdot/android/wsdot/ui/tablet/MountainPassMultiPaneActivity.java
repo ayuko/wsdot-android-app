4d4fecf6505dd90d11dafbc8b6da6e52e2ae8ac4
Wayne Dyck <dyckw@wsdot.wa.gov>
2012-03-05 15:51:38 -0800

    Initial Mountain Pass activity and layout for tablets.

--- src/gov/wa/wsdot/android/wsdot/ui/tablet/MountainPassMultiPaneActivity.java
+++ src/gov/wa/wsdot/android/wsdot/ui/tablet/MountainPassMultiPaneActivity.java
@@ -1,12 +1,15 @@
 package gov.wa.wsdot.android.wsdot.ui.tablet;
 
-import android.os.Bundle;
+import gov.wa.wsdot.android.wsdot.R;
 import gov.wa.wsdot.android.wsdot.ui.BaseMultiPaneActivity;
+import android.os.Bundle;
 
 public class MountainPassMultiPaneActivity extends BaseMultiPaneActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	}
+		
+		setContentView(R.layout.activity_mountainpass);
+	}	
 }
